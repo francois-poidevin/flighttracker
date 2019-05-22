c2c80202b9d58e5ccbcabef9dfc3ad8527da4a66
Poidevin Merdrignac, Francois <francois.podevin@continental-corporation.com>
2019-05-22 15:06:52 +0200

    create worker component, initiate the storage stucture for API response

---
+++ src/root.go
@@ -0,0 +1,73 @@
+package src
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type FlightData struct {
+	flightId         int
+	iCAO24BITADDRESS string
+	lat              float32
+	lon              float32
+	track            int //degree to the destination
+	altitude         int
+	groundSpeed      int    //kts
+	unknown1         string //not describe yet
+	transpondeurType string
+	aircraftType     string
+	immatriculation1 string
+	timeStamp        uint
+	origine          string
+	destination      string
+	unknown2         string
+	verticalSpeed    int
+	immatriculation2 string
+	unknown3         int
+	company          string
+}
+
+//Execute - start the worker
+func Execute() {
+	//Loop each 5 secondes for working
+	d := 5 * time.Second
+	for x := range time.Tick(d) {
+		storeDataOnFile(x)
+	}
+}
+
+func storeDataOnFile(t time.Time) {
+	f, err := os.OpenFile("data.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+	w := bufio.NewWriter(f)
+
+	//Made the HTTP request
+	resp, errHTTPGet := http.Get("https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=43.79,43.53,1.23,2.03&faa=1&satellite=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1")
+	if errHTTPGet != nil {
+		panic(errHTTPGet)
+	}
+	defer resp.Body.Close()
+
+	body, errRead := ioutil.ReadAll(resp.Body)
+	if errRead != nil {
+		panic(errRead)
+	}
+
+	fmt.Println(string(body))
+
+	n4, errWS := w.WriteString(t.String() + "\n" + string(body) + "\n====================================\n")
+	if errWS != nil {
+		panic(errWS)
+	}
+	w.Flush()
+	fmt.Printf("wrote %d bytes\n", n4)
+}
