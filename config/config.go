57ecfebb4f90ce4b3628abda3efe4743c7d66518
francois.poidevin@yahoo.fr <francois.podevin@continental-corporation.com>
2021-08-10 11:16:46 +0200

    doc: some run documentation improvments

--- config/config.go
+++ config/config.go
@@ -12,8 +12,8 @@ type Configuration struct {
 	} `toml:"Log" comment:"###############################\n Logs Settings \n##############################"`
 
 	Flighttracker struct {
-		Bbox       string             `toml:"bbox" default:"43.52,1.32^43.70,1.69" comment:"tracking bbox"`
-		Refresh    int                `toml:"refresh" default:"5" comment:"refresh timing"`
+		Bbox       string             `toml:"bbox" default:"43.52,1.32^43.70,1.69" comment:"tracking bbox (Lat/Lon)"`
+		Refresh    int                `toml:"refresh" default:"5" comment:"refresh timing in second"`
 		Sinkertype string             `toml:"sinkertype" default:"FILE" comment:"the sinker Type use (STDOUT|FILE|DB)"`
 		File       file.Configuration `toml:"file" comment:"###############################\n file sinker configuration \n##############################"`
 		Postgres   db.Configuration   `toml:"postgres" comment:"###############################\n postgres sinker configuration \n##############################"`
